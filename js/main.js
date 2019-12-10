1a2da5b0f12fbf52559f2e388cf19f1f12c4f9a9
Andie Swift <andie.swift5@gmail.com>
2019-12-10 13:16:12 -0800

    pulled images from pixabay

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ var geoLocation = {}
 
 function initializeApp(){
   getLocation();
+  getImg("sunny")
 }
 
 //uses the window.navigator to grab users location
@@ -18,6 +19,28 @@ function getLocation(){
 function handleSuccessLocation(response){
   geoLocation.lat = parseInt(response.coords.latitude);
   geoLocation.long = parseInt(response.coords.longitude);
-  console.log(geoLocation);
   return geoLocation;
+}
+
+function getImg(keyword){
+  var imgObj = {
+    url: "https://pixabay.com/api/",
+    method: "GET",
+    data:{
+      key:"14581706-5fa585255d8b483110ef3bfe9",
+      q: keyword
+    },
+    success: handleSuccessImg,
+    error: console.log
+  };
+
+  $.ajax(imgObj);
+}
+
+function handleSuccessImg(response){
+  var random = parseInt(Math.random()*20);
+  var url = response.hits[random].largeImageURL;
+
+  $("img").attr("src", url);
+
 }
